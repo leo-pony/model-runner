4c46f589d7ded25f77c4b1b65b2d1277fd3bf60d
Dorin Geman <dorin.geman@docker.com>
2025-03-14 11:13:21 +0200

    inference: Supported on darwin/arm64 only
    Signed-off-by: Dorin Geman <dorin.geman@docker.com>

--- pkg/inference/platform.go
+++ pkg/inference/platform.go
@@ -7,6 +7,5 @@ import (
 // Supported returns whether or not the current host platform is supported for
 // inference operations. This support should expand over time.
 func Supported() bool {
-	return (runtime.GOOS == "windows" && runtime.GOARCH == "amd64") ||
-		(runtime.GOOS == "darwin" && runtime.GOARCH == "arm64")
+	return runtime.GOOS == "darwin" && runtime.GOARCH == "arm64"
 }
