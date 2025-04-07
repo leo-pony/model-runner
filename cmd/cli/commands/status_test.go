c719b75d40aaaaf82e945b698b8856311717955b
Dorin Geman <dorin.geman@docker.com>
2025-04-07 19:29:13 +0300

    status: Add mock and UTs
    Signed-off-by: Dorin Geman <dorin.geman@docker.com>

---
+++ cmd/cli/commands/status_test.go
@@ -0,0 +1,113 @@
+package commands
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/docker/cli/cli-plugins/hooks"
+	"github.com/docker/model-cli/desktop"
+	mockdesktop "github.com/docker/model-cli/mocks"
+	"github.com/docker/pinata/common/pkg/inference"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func TestStatus(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockBody := io.NopCloser(strings.NewReader(""))
+
+	tests := []struct {
+		name           string
+		doResponse     *http.Response
+		doErr          error
+		expectExit     bool
+		expectedErr    error
+		expectedOutput string
+	}{
+		{
+			name:           "running",
+			doResponse:     &http.Response{StatusCode: http.StatusOK, Body: mockBody},
+			doErr:          nil,
+			expectExit:     false,
+			expectedErr:    nil,
+			expectedOutput: "Docker Model Runner is running\n",
+		},
+		{
+			name:        "not running",
+			doResponse:  &http.Response{StatusCode: http.StatusServiceUnavailable, Body: mockBody},
+			doErr:       nil,
+			expectExit:  true,
+			expectedErr: nil,
+			expectedOutput: func() string {
+				buf := new(bytes.Buffer)
+				fmt.Fprintln(buf, "Docker Model Runner is not running")
+				hooks.PrintNextSteps(buf, []string{enableViaCLI, enableViaGUI})
+				return buf.String()
+			}(),
+		},
+		{
+			name:       "request with error",
+			doResponse: &http.Response{StatusCode: http.StatusInternalServerError, Body: mockBody},
+			doErr:      nil,
+			expectExit: false,
+			expectedErr: handleClientError(
+				fmt.Errorf("unexpected status code: %d", http.StatusInternalServerError),
+				"Failed to get Docker Model Runner status",
+			),
+			expectedOutput: "",
+		},
+		{
+			name:       "failed request",
+			doResponse: nil,
+			doErr:      fmt.Errorf("failed to make request"),
+			expectExit: false,
+			expectedErr: handleClientError(
+				fmt.Errorf("error querying %s: %w", inference.ModelsPrefix, fmt.Errorf("failed to make request")),
+				"Failed to get Docker Model Runner status",
+			),
+			expectedOutput: "",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			client := mockdesktop.NewMockDockerHttpClient(ctrl)
+			req, err := http.NewRequest(http.MethodGet, desktop.URL(inference.ModelsPrefix), nil)
+			require.NoError(t, err)
+			client.EXPECT().Do(req).Return(test.doResponse, test.doErr)
+
+			originalOsExit := osExit
+			exitCalled := false
+			osExit = func(code int) {
+				exitCalled = true
+				require.Equal(t, 1, code, "Expected exit code to be 1")
+			}
+			defer func() { osExit = originalOsExit }()
+
+			cmd := newStatusCmd(desktop.New(client))
+			buf := new(bytes.Buffer)
+			cmd.SetOut(buf)
+			cmd.SetErr(buf)
+
+			err = cmd.Execute()
+			if test.expectExit {
+				require.True(t, exitCalled, "Expected os.Exit to be called")
+			} else {
+				require.False(t, exitCalled, "Did not expect os.Exit to be called")
+			}
+			if test.expectedErr != nil {
+				require.Error(t, err)
+				require.EqualError(t, err, test.expectedErr.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, test.expectedOutput, buf.String())
+			}
+		})
+	}
+}
--- cmd/cli/go.mod
+++ cmd/cli/go.mod
@@ -10,7 +10,9 @@ require (
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.9.1
+	github.com/stretchr/testify v1.10.0
 	go.opentelemetry.io/otel v1.34.0
+	go.uber.org/mock v0.5.0
 )
 
 require (
@@ -21,6 +23,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/containerd/stargz-snapshotter/estargz v0.16.3 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/docker/distribution v2.8.3+incompatible // indirect
 	github.com/docker/docker-credential-helpers v0.9.3 // indirect
@@ -54,6 +57,7 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/prometheus/client_golang v1.20.5 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
@@ -92,5 +96,6 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250219182151-9fdb1cabc7b2 // indirect
 	google.golang.org/grpc v1.70.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	mvdan.cc/xurls/v2 v2.5.0 // indirect
 )
