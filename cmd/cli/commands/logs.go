8c11732c6aae607b467cdb7d27bffd260c747279
Dorin Geman <dorin.geman@docker.com>
2025-04-15 17:11:34 +0300

    logs: Fetch inference engines logs
    Signed-off-by: Dorin Geman <dorin.geman@docker.com>

--- cmd/cli/commands/logs.go
+++ cmd/cli/commands/logs.go
@@ -1,18 +1,22 @@
 package commands
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"runtime"
 
 	"github.com/docker/model-cli/commands/completion"
 	"github.com/nxadm/tail"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 )
 
 func newLogsCmd() *cobra.Command {
-	var follow bool
+	var follow, noEngines bool
 	c := &cobra.Command{
 		Use:   "logs [OPTIONS]",
 		Short: "Fetch the Docker Model Runner logs",
@@ -32,21 +36,62 @@ func newLogsCmd() *cobra.Command {
 				return fmt.Errorf("unsupported OS: %s", runtime.GOOS)
 			}
 
-			t, err := tail.TailFile(
-				logFilePath, tail.Config{Follow: follow, ReOpen: follow},
-			)
-			if err != nil {
-				return err
+			ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+			defer cancel()
+
+			g, ctx := errgroup.WithContext(ctx)
+
+			g.Go(func() error {
+				t, err := tail.TailFile(
+					logFilePath, tail.Config{Follow: follow, ReOpen: follow},
+				)
+				if err != nil {
+					return err
+				}
+				for {
+					select {
+					case line, ok := <-t.Lines:
+						if !ok {
+							return nil
+						}
+						fmt.Println(line.Text)
+					case <-ctx.Done():
+						return t.Stop()
+					}
+				}
+			})
+
+			if follow && !noEngines {
+				// Show inference engines logs if `follow` is enabled
+				// and the engines logs have not been skipped by setting `--no-engines`.
+				g.Go(func() error {
+					t, err := tail.TailFile(
+						filepath.Join(filepath.Dir(logFilePath), "inference-llama.cpp-server.log"),
+						tail.Config{Location: &tail.SeekInfo{Offset: 0, Whence: io.SeekEnd}, Follow: follow, ReOpen: follow},
+					)
+					if err != nil {
+						return err
+					}
+
+					for {
+						select {
+						case line, ok := <-t.Lines:
+							if !ok {
+								return nil
+							}
+							fmt.Println(line.Text)
+						case <-ctx.Done():
+							return t.Stop()
+						}
+					}
+				})
 			}
 
-			for line := range t.Lines {
-				fmt.Println(line.Text)
-			}
-
-			return nil
+			return g.Wait()
 		},
 		ValidArgsFunction: completion.NoComplete,
 	}
 	c.Flags().BoolVarP(&follow, "follow", "f", false, "Follow log output")
+	c.Flags().BoolVar(&noEngines, "no-engines", false, "Skip inference engines logs")
 	return c
 }
