96faaee31806bce971c2b302f4ce8398529631c5
Dorin Geman <dorin.geman@docker.com>
2025-05-09 16:27:40 +0300

    Use context-based Docker client
    Signed-off-by: Dorin Geman <dorin.geman@docker.com>

--- cmd/cli/commands/root.go
+++ cmd/cli/commands/root.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/docker/docker/client"
 	"github.com/docker/model-cli/desktop"
-	"github.com/docker/pinata/common/pkg/engine"
-	"github.com/docker/pinata/common/pkg/paths"
 	"github.com/spf13/cobra"
 )
 
@@ -16,10 +13,7 @@ func NewRootCmd() *cobra.Command {
 		Use:   "model",
 		Short: "Docker Model Runner",
 	}
-	dockerClient, err := client.NewClientWithOpts(
-		// TODO: Make sure it works while running in Windows containers mode.
-		client.WithHost(paths.HostServiceSockets().DockerHost(engine.Linux)),
-	)
+	dockerClient, err := desktop.NewDockerClient(rootCmd.Context())
 	if err != nil {
 		fmt.Println("Failed to create Docker client:", err)
 		os.Exit(1)
