0b21e5371f2f10f1651c97d08e0050600f714cb0
Dorin Geman <dorin.geman@docker.com>
2025-06-05 17:55:20 +0300

    standalone: Fix default port comment for Cloud environments
    Signed-off-by: Dorin Geman <dorin.geman@docker.com>

--- cmd/cli/pkg/standalone/ports.go
+++ cmd/cli/pkg/standalone/ports.go
@@ -5,6 +5,6 @@ const (
 	// controller will listen for requests in Moby environments.
 	DefaultControllerPortMoby = 12434
 	// DefaultControllerPortCloud is the default TCP port on which the
-	// standalone controller will listen for requests in Moby environments.
+	// standalone controller will listen for requests in Cloud environments.
 	DefaultControllerPortCloud = 12435
 )
