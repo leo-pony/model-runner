d375147d88ecf5d395f562bcf7497f0ffc5c3545
Eric Curtin <eric.curtin@docker.com>
2025-10-09 12:01:02 +0100

    Use lunar vulkan
    I think we have a mismatch here
    
    Signed-off-by: Eric Curtin <eric.curtin@docker.com>

--- scripts/apt-install.sh
+++ scripts/apt-install.sh
@@ -5,8 +5,16 @@ main() {
 
   apt-get update
   local packages=("ca-certificates")
-  if [ "${LLAMA_SERVER_VARIANT}" = "generic" ] || [ "${LLAMA_SERVER_VARIANT}" = "cpu" ]; then
-      packages+=("libvulkan1")
+  if [ "$LLAMA_SERVER_VARIANT" = "generic" ] || [ "$LLAMA_SERVER_VARIANT" = "cpu" ]; then
+    # Install Vulkan SDK
+    local vulkan_version=1.4.321.1
+    local arch
+    arch=$(uname -m)
+    wget -qO /tmp/vulkan-sdk.tar.xz https://sdk.lunarg.com/sdk/download/$vulkan_version/linux/vulkan-sdk-linux-"$arch"-$vulkan_version.tar.xz
+    mkdir -p /opt/vulkan
+    tar -xf /tmp/vulkan-sdk.tar.xz -C /tmp --strip-components=1
+    mv /tmp/"$arch"/* /opt/vulkan/
+    rm -rf /tmp/*
   fi
 
   apt-get install -y "${packages[@]}"
